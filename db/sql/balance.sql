9c2d4aa9d580f36d2dd1a0d2ad408454e669e7ce
m.kishi <m.kishi.5@gmail.com>
2015-04-08 23:09:30 +0900

    ADD:収支合計

--- db/sql/balance.sql
+++ db/sql/balance.sql
@@ -16,6 +16,18 @@ WHERE
     type NOT IN ('PRVI','PRVO')
 GROUP BY
     year
+UNION ALL
+SELECT
+    9999                                                                   AS year
+  , SUM(CASE type WHEN 'EARN' THEN cost ELSE 0 END)                        AS earn
+  , SUM(CASE type WHEN 'BNUS' THEN cost ELSE 0 END)                        AS bnus
+  , SUM(CASE WHEN type NOT IN ('EARN','BNUS','SPCL') THEN cost ELSE 0 END) AS expense
+  , SUM(CASE type WHEN 'SPCL' THEN cost ELSE 0 END)                        AS special
+  , SUM(CASE WHEN type IN ('EARN','BNUS') THEN cost ELSE -cost END)        AS balance
+FROM
+    expenses
+WHERE
+    type NOT IN ('PRVI','PRVO')
 ORDER BY
     year
 ;
