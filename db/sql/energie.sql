6d7e5141bbc942ed1dc51af4a7e45be5415d4b59
m.kishi <m.kishi.5@gmail.com>
2016-05-01 21:41:17 +0900

    REL:Heroku対応

--- db/sql/energie.sql
+++ db/sql/energie.sql
@@ -1,27 +1,27 @@
 SELECT
-    CASE WHEN STRFTIME('%m', date) > '03'
+    CASE WHEN TO_CHAR(date, 'MM') > '03'
       THEN
-        STRFTIME('%Y', date) + 0
+        TO_CHAR(date, 'YYYY')::integer + 0
       ELSE
-        STRFTIME('%Y', date) - 1
-    END                                                           AS year
+        TO_CHAR(date, 'YYYY')::integer - 1
+    END                                                          AS year
   , MAX(CASE WHEN name = '電気代' THEN 'ELE'
              WHEN name = 'ガス代' THEN 'GAS'
              WHEN name = '水道代' THEN 'WTR'
                                   ELSE 'ERR'
-        END)                                                      AS type
-  , SUM(CASE STRFTIME('%m', date) WHEN '04' THEN cost ELSE 0 END) AS mnth04
-  , SUM(CASE STRFTIME('%m', date) WHEN '05' THEN cost ELSE 0 END) AS mnth05
-  , SUM(CASE STRFTIME('%m', date) WHEN '06' THEN cost ELSE 0 END) AS mnth06
-  , SUM(CASE STRFTIME('%m', date) WHEN '07' THEN cost ELSE 0 END) AS mnth07
-  , SUM(CASE STRFTIME('%m', date) WHEN '08' THEN cost ELSE 0 END) AS mnth08
-  , SUM(CASE STRFTIME('%m', date) WHEN '09' THEN cost ELSE 0 END) AS mnth09
-  , SUM(CASE STRFTIME('%m', date) WHEN '10' THEN cost ELSE 0 END) AS mnth10
-  , SUM(CASE STRFTIME('%m', date) WHEN '11' THEN cost ELSE 0 END) AS mnth11
-  , SUM(CASE STRFTIME('%m', date) WHEN '12' THEN cost ELSE 0 END) AS mnth12
-  , SUM(CASE STRFTIME('%m', date) WHEN '01' THEN cost ELSE 0 END) AS mnth01
-  , SUM(CASE STRFTIME('%m', date) WHEN '02' THEN cost ELSE 0 END) AS mnth02
-  , SUM(CASE STRFTIME('%m', date) WHEN '03' THEN cost ELSE 0 END) AS mnth03
+        END)                                                     AS type
+  , SUM(CASE TO_CHAR(date, 'MM') WHEN '04' THEN cost ELSE 0 END) AS mnth04
+  , SUM(CASE TO_CHAR(date, 'MM') WHEN '05' THEN cost ELSE 0 END) AS mnth05
+  , SUM(CASE TO_CHAR(date, 'MM') WHEN '06' THEN cost ELSE 0 END) AS mnth06
+  , SUM(CASE TO_CHAR(date, 'MM') WHEN '07' THEN cost ELSE 0 END) AS mnth07
+  , SUM(CASE TO_CHAR(date, 'MM') WHEN '08' THEN cost ELSE 0 END) AS mnth08
+  , SUM(CASE TO_CHAR(date, 'MM') WHEN '09' THEN cost ELSE 0 END) AS mnth09
+  , SUM(CASE TO_CHAR(date, 'MM') WHEN '10' THEN cost ELSE 0 END) AS mnth10
+  , SUM(CASE TO_CHAR(date, 'MM') WHEN '11' THEN cost ELSE 0 END) AS mnth11
+  , SUM(CASE TO_CHAR(date, 'MM') WHEN '12' THEN cost ELSE 0 END) AS mnth12
+  , SUM(CASE TO_CHAR(date, 'MM') WHEN '01' THEN cost ELSE 0 END) AS mnth01
+  , SUM(CASE TO_CHAR(date, 'MM') WHEN '02' THEN cost ELSE 0 END) AS mnth02
+  , SUM(CASE TO_CHAR(date, 'MM') WHEN '03' THEN cost ELSE 0 END) AS mnth03
 FROM
     expenses
 WHERE
